0d50ba8ae8ef643f52edbbf8b2ca0da9b3295fa8
Chris Bennett <chris@ceegeebee.com>
2014-11-11 19:35:43 +1030

    Initial add of Tracer 1.6

---
+++ Casks/tracer.rb
@@ -0,0 +1,10 @@
+class Tracer < Cask
+  version '1.6'
+  sha256 '1de9b94ab3d9b981e822c668dba54e5e5938794b7ed75bf0726f5eca246fe195'
+
+  url 'http://tree.bio.ed.ac.uk/download.php?id=88'
+  homepage 'http://tree.bio.ed.ac.uk/software/tracer/'
+  license :unknown
+
+  app 'Tracer v1.6.app'
+end
