0bd59a93b5dc4beb1da4a39774251860af88d6d3
Chris Bennett <chris@ceegeebee.com>
2014-11-11 19:32:17 +1030

    Revert back to explicit version.  No way to override a Cask that reports is version as 'latest' yet.

--- Casks/figtree.rb
+++ Casks/figtree.rb
@@ -1,5 +1,5 @@
 class Figtree < Cask
-  version :latest
+  version '1.4.2'
   sha256 '3b97cb9b57bbce2466d850faac699128b7b72310b094cf9260c1a172561674f1'
 
   url 'http://tree.bio.ed.ac.uk/download.php?id=90'
